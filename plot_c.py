fc0ba4bd4dc7f678abbea73e5d9c90b1cafdf655
София Сидукова <sofi.horizon@gmail.com>
2023-01-26 11:36:55 +0200

    Initial Commit Racoon

--- plot_c.py
+++ plot_c.py
@@ -9,3 +9,29 @@ df = pd.read_csv('data.csv')
 df.plot()
 
 plt.show()
+
+
+"""
+SCATTER PLOT
+Specify that you want a scatter plot with the 'kind' argument:
+# kind = 'scatter'
+A scatter plot needs an x- ans an y-axis.
+"""
+# in the example below we will use "Diration' for the x-axis and
+# "Calories" for the y-axis.
+
+df2 = pd.read_csv('data.csv')
+
+df2.plot(kind='scatter', x='Duration', y='Calories')
+plt.show()
+
+"""
+HISTOGRAM
+# kind = 'hist'
+A histogram needs only one column.
+"""
+# in the example below we will use "Duration" column to create the
+# histogram
+
+df3 = pd.read_csv('data.csv')
+df3["Duration"].plot(kind='hist')
