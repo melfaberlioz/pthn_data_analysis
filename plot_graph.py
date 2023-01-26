53f58445210bf61d6fc22babea560b8cc64d41d3
София Сидукова <sofi.horizon@gmail.com>
2023-01-26 08:57:55 +0200

    Initial Commit Racoon

--- plot_graph.py
+++ plot_graph.py
@@ -4,3 +4,9 @@ import numpy as np
 
 gasPrice = pd.read_csv('gas_prices.csv')
 plt.plot(gasPrice.Year, gasPrice.Canada)
+
+# adding titles
+plt.suptitle('Gas Price Comparison')
+plt.title('Canada', fontdict={'fontsize': 15, 'fontweight': 'bold'})
+plt.xlabel('Year')
+plt.ylabel('Price in USD')
